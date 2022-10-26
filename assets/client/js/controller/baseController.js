0ed6f084fc4c469bdc657406b0ca069ac5a69612
QuocHuyTruong <minhhuytruong10@gmail.com>
2022-10-26 16:29:18 +0700

    cap nhat giao dien trang chu

---
+++ assets/client/js/controller/baseController.js
@@ -0,0 +1,37 @@
+﻿var common = {
+    init: function () {
+        common.registerEvent();
+    },
+    registerEvent: function () {
+        $(".txtSearch").autocomplete({
+            minLength: 1,
+            source: function (request, response) {
+                $.ajax({
+                    url: "/Book/ListName",
+                    dataType: "json",
+                    data: {
+                        q: request.term
+                    },
+                    success: function (res) {
+                        console.log(res.data)
+                        response(res.data);
+                    }
+                });
+            },
+            appendTo: "#search_mini_form",
+            focus: function (event, ui) {
+                $(".txtSearch").val(ui.item.label);
+                return true;
+            },
+            select: function (event, ui) {
+                $(".txtSearch").val(ui.item.label);
+                return true;
+            }
+        }).autocomplete("instance")._renderItem = function (ul, item) {
+            return $('<li>')
+                .append("<a>" + item.label + "</a>")
+                .appendTo(ul);
+        };
+    }
+}
+common.init();
---
+++ assets/client/js/controller/cartController.js
@@ -0,0 +1,96 @@
+﻿var cart = {
+    init: function () {
+        cart.regEvents();
+    },
+    regEvents: function () {
+        $('#btnContinue').off('click').on('click', function () {
+            window.location.href = "/danh-muc";
+        });
+        $('#btnPayment').off('click').on('click', function () {
+            window.location.href = "/gio-hang/thanh-toan";
+        });
+        $('#btnUpdate').off('click').on('click', function () {
+            var listProduct = $('.txtQuantity');
+            var cartList = [];
+            $.each(listProduct, function (i, item) {
+                cartList.push({
+                    Quantity: $(item).val(),
+                    book: {
+                        BookID: $(item).data('bookid')
+                    }
+                });
+            });
+
+            $.ajax({
+                url: '/Cart/Update',
+                data: { cartModel: JSON.stringify(cartList) },
+                dataType: 'json',
+                type: 'POST',
+                success: function (res) {
+                    if (res.status == true) {
+                        window.location.href = "/gio-hang";
+                    }
+                }
+            })
+        });
+
+        $('#btnDeleteAll').off('click').on('click', function () {
+
+            $.ajax({
+                url: '/Cart/DeleteAll',
+                dataType: 'json',
+                type: 'POST',
+                success: function (res) {
+                    if (res.status == true) {
+                        window.location.href = "/gio-hang";
+                    }
+                }
+            })
+        });
+
+        $('.btn-delete').off('click').on('click', function (e) {
+            e.preventDefault();
+            $.ajax({
+                data: { id: $(this).data('id') },
+                url: '/Cart/Delete',
+                dataType: 'json',
+                type: 'POST',
+                success: function (res) {
+                    if (res.status == true) {
+                        window.location.href = "/gio-hang";
+                    }
+                }
+            })
+        });
+    }
+}
+
+cart.init();
+
+function addItem(bookID) {
+    $.ajax({
+        type: 'GET',
+        url: '/Cart/Increase',
+        data: { bookID: bookID },
+        success: function (result) {
+            $('#cart-table-container').html(result)
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
+
+function subItem(bookID) {
+    $.ajax({
+        type: 'GET',
+        url: '/Cart/Decrease',
+        data: { bookID: bookID },
+        success: function (result) {
+            $('#cart-table-container').html(result)
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
